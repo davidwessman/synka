4f6714b014e6aad81ee5ca3931936d97ccb15aec
David Wessman <david.wesmn@gmail.com>
2018-01-10 13:48:28 +0100

    Setups locale

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,7 @@ module OpeningHours
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales',
+                                                 '**', '*.yml')]
   end
 end
