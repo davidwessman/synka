667071c07530a08e588d7f36cdc7fc6e789851ac
David Wessman <david.wesmn@gmail.com>
2018-01-24 17:17:54 +0100

    Fix tests

--- test/controllers/shifts_controller_test.rb
+++ test/controllers/shifts_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class ShiftsControllerTest < ActionDispatch::IntegrationTest
   test 'should require login' do
-    patch(account_shift_path(shifts(:one)), xhr: true)
+    shift = shifts(:one)
+    patch(account_location_shift_path(shift.connection.location, shift),
+          xhr: true)
     assert_response(401)
   end
 end
