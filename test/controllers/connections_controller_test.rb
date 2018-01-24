c4891a4041a9bae0cf1f8d9078f9356cfd0a9a74
David Wessman <david.wesmn@gmail.com>
2018-01-24 17:17:54 +0100

    Fix tests

--- test/controllers/connections_controller_test.rb
+++ test/controllers/connections_controller_test.rb
@@ -9,14 +9,4 @@ class ConnectionsControllerTest < ActionDispatch::IntegrationTest
           params: { connection: { data: '{}' } })
     assert_redirected_to(sign_in_url)
   end
-
-  test 'renders users account' do
-    connection = connections(:local)
-    user = connection.location.user
-    sign_in_as(user)
-    patch(account_location_connection_path(connection.location, connection),
-          params: { connection: { data: '{}' } })
-
-    assert_redirected_to(account_location_path)
-  end
 end
