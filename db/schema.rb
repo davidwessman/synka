9e0b392d0909cf7215a70bf8f9825c0c62c7bbc6
David Wessman <david.wesmn@gmail.com>
2018-03-06 17:31:16 +0100

    Webpacker: regenerate binstubs
    - Based on
    https://github.com/rails/webpacker/blob/master/CHANGELOG.md#breaking-changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124130837) do
+ActiveRecord::Schema.define(version: 2018_01_24_130837) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
   create_table "connections", force: :cascade do |t|
     t.uuid "location_id"
