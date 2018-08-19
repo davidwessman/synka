966e291592766474a83ff2e61bb9ab102d5c4d33
David Wessman <david.wesmn@gmail.com>
2018-08-19 15:44:49 +0200

    Local dev url and test setup

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Koala.configure do |config|
-  config.access_token = ENV.fetch('FB_ACCESS_TOKEN', '')
-  config.app_access_token = ENV.fetch('FB_APP_ACCESS_TOKEN', '')
   config.app_id = ENV.fetch('FB_APP_ID', '')
   config.app_secret = ENV.fetch('FB_APP_SECRET', '')
   # See Koala::Configuration for more options, including details on
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,23 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
+                       options: { args: ['disable-notifications'] }
+
+  def setup
+    Rails.application.routes.default_url_options[:host] = Capybara.current_session.server.host
+    Rails.application.routes.default_url_options[:port] = Capybara.current_session.server.port
+  end
+
+  # override API MOCK
+  def api_setup; end
+
+  def sign_in_as(user)
+    visit(sign_in_url)
+    fill_in('email', with: user.email)
+    fill_in('password', with: 'passpass')
+    within('form') do
+      click_on('Sign in')
+    end
+  end
 end
