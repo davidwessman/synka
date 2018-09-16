f85f521fe95acd7787190bc6977a992d5ce02a75
David Wessman <david.wesmn@gmail.com>
2018-09-16 21:34:21 +0200

    Adds mailer and email layout

---
+++ app/mailers/checks_mailer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ChecksMailer < ApplicationMailer
+  def failed(location_id)
+    @location = Location.includes(:user, connections: :shifts).find(location_id)
+    mail(to: @location.user.email, subject: I18n.t('checks.failed'))
+  end
+end
