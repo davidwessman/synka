c0daa1f537bf9f5aa2a600d6dd908e15420f49ff
David Wessman <david.wesmn@gmail.com>
2018-10-14 17:02:42 +0200

    Updates system tests to reflect landing page

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,13 +3,22 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
-                       options: { args: ['disable-notifications'] }
-  setup :set_host
+  driven_by(
+    :selenium,
+    using: :chrome, screen_size: [1400, 1400],
+    options: {
+      options: Selenium::WebDriver::Chrome::Options.new(
+        args: %w[disable-notifications disable-gpu]
+      )
+    }
+  )
+  setup(:set_host)
 
   def set_host
-    Rails.application.routes.default_url_options[:host] = Capybara.current_session.server.host
-    Rails.application.routes.default_url_options[:port] = Capybara.current_session.server.port
+    Rails.application.routes.default_url_options[:host] = \
+      Capybara.current_session.server.host
+    Rails.application.routes.default_url_options[:port] = \
+      Capybara.current_session.server.port
   end
 
   # override API MOCK
