e6a846659d0dc257d57a851241b231902523dd8b
wsmn <arthurwessman@gmail.com>
2018-10-17 22:49:33 +0200

    Mobile menu implemented.

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,9 +1,12 @@
 import shiftStatus from 'application/shift_status';
+import menuDisplay from 'application/menu';
 
 document.addEventListener('turbolinks:load', () => {
   shiftStatus.initialize();
+  menuDisplay.initialize();
 });
 
 document.addEventListener('turbolinks:before-cache', () => {
   shiftStatus.tearDown();
+  menuDisplay.tearDown();
 });
