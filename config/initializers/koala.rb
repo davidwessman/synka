466a22766fd5ce08ce1e15b49c7986052f44ca39
David Wessman <david.wesmn@gmail.com>
2019-04-28 17:38:44 +0200

    Remove unused code

--- config/initializers/koala.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Koala.configure do |config|
-  config.app_id = ENV.fetch("FB_APP_ID", "")
-  config.app_secret = ENV.fetch("FB_APP_SECRET", "")
-  # See Koala::Configuration for more options, including details on
-  # how to send requests through your own proxy servers.
-end
