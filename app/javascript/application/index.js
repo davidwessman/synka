e8932d56edb6f50e38e454d286c0d9588eb1a50a
David Wessman <david.wesmn@gmail.com>
2019-07-02 18:06:36 +0200

    Remove unused javascript

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,12 +1,9 @@
-import shiftStatus from 'application/shift_status';
 import menuDisplay from 'application/menu';
 
 document.addEventListener('turbolinks:load', () => {
-  shiftStatus.initialize();
   menuDisplay.initialize();
 });
 
 document.addEventListener('turbolinks:before-cache', () => {
-  shiftStatus.tearDown();
   menuDisplay.tearDown();
 });
