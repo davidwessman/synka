01907989010fdbf2a14b1066b62b6cbc8b4e5d05
David Wessman <david.wesmn@gmail.com>
2020-07-18 22:58:33 +0200

    Remove unused javascript

--- app/javascript/application/index.js
+++
@@ -1,9 +0,0 @@
-import menuDisplay from 'application/menu';
-
-document.addEventListener('turbolinks:load', () => {
-  menuDisplay.initialize();
-});
-
-document.addEventListener('turbolinks:before-cache', () => {
-  menuDisplay.tearDown();
-});
